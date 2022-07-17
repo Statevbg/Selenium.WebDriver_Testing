fb00f72bd9c2032097f2f0c1f74613629e973779
Dimitar Statev <75937149+Statevbg@users.noreply.github.com>
2022-07-17 14:39:54 +0300

    Add files via upload

---
+++ WebDriverExercises/Automationpractice.com/Tests/Base_Test.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automationpractice.com.Tests
+{
+    public class Base_Test
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void ShutDown()
+        {
+            driver.Quit();
+        }
+    }
+}
