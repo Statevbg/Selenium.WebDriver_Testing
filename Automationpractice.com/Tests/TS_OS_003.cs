6885e37c9171821c30dc83a0353f05a1735e1249
Dimitar Statev <75937149+Statevbg@users.noreply.github.com>
2022-07-21 22:28:45 +0300

    Add files via upload

---
+++ Automationpractice.com/Tests/TS_OS_003.cs
@@ -0,0 +1,38 @@
+﻿using Automationpractice.com.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automationpractice.com.Tests
+{
+    public class TS_OS_003 : Base_Test
+    {
+        [TestCase("dress")]
+        [TestCase("summer")]
+        public void Test_SearchBox_ValidKeywords(string keyword)
+        {
+
+            // Arrange
+            var search = new SearchBox(driver);
+            // Act
+            search.Search_Keyword(keyword);
+            var result = search.checkResult.Text;
+            // Assert
+            Assert.That(result, Is.GreaterThan("0"));
+        }
+        [TestCase("!@#$%^&*()_+{}")]
+        [TestCase("1234asdASD")]
+        public void Test_SearchBox_InvalidKeywords(string keyword)
+        {
+            // Arrange
+            var search = new SearchBox(driver);
+            // Act
+            search.Search_Keyword(keyword);
+            var result = search.checkResult.Text;
+            // Assert
+            Assert.That(result, Is.EqualTo("0 results have been found."));
+        }
+    }
+}
